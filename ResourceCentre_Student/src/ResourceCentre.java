b65c6bb42b6b96f5bd3dae38665eeee2d6f89da1
20019246 <20019246@DESKTOP-VNCNCPP>
2021-07-26 15:06:06 +0800

    20019246 save changes for inputChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -184,7 +184,6 @@ public class ResourceCentre {
 	
 	public static Chromebook inputChromebook() {	
 		Chromebook cb =null;
-		// write your code here
 		String tag = Helper.readString("Enter asset tag > ");
 		String description = Helper.readString("Enter description > ");
 		String os = Helper.readString("Enter operating system > ");
